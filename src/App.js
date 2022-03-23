0b168b5b45232427e074358c959c471729f6cf61
Aryan Tayal <bookworms@ariansh.com>
2022-03-23 06:27:09 +0530

    create PhotoGrid component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useState, useEffect } from "react";
-import Photo from "./Photo.js";
-import axios from "axios";
+import PhotoGrid from "./PhotoGrid.js";
 import "./App.css";
 
 App.defaultProps = {
@@ -8,41 +6,7 @@ App.defaultProps = {
 };
 
 function App({ photoNumber }) {
-  const [photos, setPhotos] = useState([]);
-
-  const createPhotos = async () => {
-    const getImg = async () => {
-      const res = await axios.get(
-        "https://api.unsplash.com/search/photos/?client_id=-NHOwRYeg475kTXWwzhOGAIUF4eQyp8kW8pnjkL-4Lg",
-        {
-          params: {
-            query: "space",
-            orientation: "squarish",
-            per_page: photoNumber,
-          },
-        }
-      );
-      setPhotos([...res.data.results]);
-    };
-
-    await getImg();
-  };
-  useEffect(() => {
-    if (photos.length === 0) createPhotos();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  return (
-    <>
-      {photos.map((p) => (
-        <Photo
-          alt={p.alt_description}
-          src={p.urls.raw}
-          key={p.id}
-          user={p.user.name}
-        />
-      ))}
-    </>
-  );
+  return <PhotoGrid photoNumber={photoNumber} />;
 }
 
 export default App;
---
+++ src/PhotoGrid.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import Photo from "./Photo.js";
+import axios from "axios";
+
+const PhotoGrid = ({ photoNumber }) => {
+  const [photos, setPhotos] = useState([]);
+
+  const createPhotos = async () => {
+    const getImg = async () => {
+      const res = await axios.get(
+        "https://api.unsplash.com/search/photos/?client_id=-NHOwRYeg475kTXWwzhOGAIUF4eQyp8kW8pnjkL-4Lg",
+        {
+          params: {
+            query: "space",
+            orientation: "squarish",
+            per_page: photoNumber,
+          },
+        }
+      );
+      setPhotos([...res.data.results]);
+    };
+    await getImg();
+  };
+  useEffect(() => {
+    if (photos.length === 0) createPhotos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return (
+    <>
+      {photos.map((p) => (
+        <Photo
+          alt={p.alt_description}
+          src={p.urls.raw}
+          key={p.id}
+          user={p.user.name}
+        />
+      ))}
+    </>
+  );
+};
+
+export default PhotoGrid;
